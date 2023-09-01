86fa967df487ca9bfd563607850bdc1196d861d3
Trevor Elkins <trevor@emergetools.com>
2023-09-01 16:57:24 -0400

    Fix regression

--- ios/Hacker News/Components/StoryRow.swift
+++ ios/Hacker News/Components/StoryRow.swift
@@ -40,7 +40,13 @@ struct StoryRow: View {
               .foregroundColor(Color.primary.opacity(0.6))
           }
           HStack {
-            let dateAndAuthor = "\(story.displayableDate) by \(story.by)"
+            let dateAndAuthor: String = {
+              if let author = story.by {
+                return "\(story.displayableDate) by \(author)"
+              } else {
+                return story.displayableDate
+              }
+            }()
             Text(dateAndAuthor)
               .font(.caption)
               .foregroundColor(Color.primary.opacity(0.6))
