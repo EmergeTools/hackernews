35a4667d375765d03e568ef2c741e55b7d15dd16
Trevor Elkins <trevor@emergetools.com>
2023-09-29 12:02:22 -0400

    Add multi-preview for snapshot testing

---
+++ android/app/src/main/kotlin/com/emergetools/hackernews/ui/annotations/SnapshotTestingPreviews.kt
@@ -0,0 +1,6 @@
+package com.emergetools.hackernews.ui.annotations
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@FontScalePreviews
+annotation class SnapshotTestingPreviews
--- android/app/src/main/kotlin/com/emergetools/hackernews/ui/items/Comment.kt
+++ android/app/src/main/kotlin/com/emergetools/hackernews/ui/items/Comment.kt
@@ -8,11 +8,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.emergetools.hackernews.network.models.Comment
 import com.emergetools.hackernews.ui.BuildItem
-import com.emergetools.hackernews.ui.annotations.FontScalePreviews
+import com.emergetools.hackernews.ui.annotations.SnapshotTestingPreviews
 
 @Composable
 fun BuildComment(
@@ -36,7 +35,7 @@ fun BuildComment(
  * Example generated snapshot test from main source set.
  * To generate a snapshot test for this preview, add the snapshot-processor as a ksp dependency.
  */
-@FontScalePreviews
+@SnapshotTestingPreviews
 @Composable
 fun CommentRow() {
   val mockComment = Comment(
