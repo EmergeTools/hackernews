bec2b3032e3d52c688c72d0fef2f55131b05a682
Trevor Elkins <trevor@emergetools.com>
2023-10-10 15:52:14 -0400

    Move to init

--- ios/Hacker News/HNApp.swift
+++ ios/Hacker News/HNApp.swift
@@ -13,12 +13,23 @@ import SwiftUI
 @main
 struct Hacker_NewsApp: App {
 
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
   @StateObject private var appState = AppViewModel()
 
   init() {
     UINavigationBar.appearance().backgroundColor = .clear
     UICollectionView.appearance().backgroundColor = .clear
+    
+    EMGReaper.sharedInstance().start(withAPIKey: "f77fb081-cfc2-4d15-acb5-18bad59c9376")
+    
+    SentrySDK.start { options in
+        options.dsn = "https://118cff4b239bd3e0ede8fd74aad9bf8f@o497846.ingest.sentry.io/4506027753668608"
+        options.debug = true // Enabled debug when first installing is always helpful
+        options.enableTracing = true
+
+        // Uncomment the following lines to add more data to your events
+        // options.attachScreenshot = true // This adds a screenshot to the error events
+        // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+    }
   }
 
   var body: some Scene {
@@ -51,26 +62,5 @@ struct Hacker_NewsApp: App {
         }
       }
     }
-  }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(
-    _ application: UIApplication, didFinishLaunchingWithOptions
-    launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool
-  {
-    EMGReaper.sharedInstance().start(withAPIKey: "f77fb081-cfc2-4d15-acb5-18bad59c9376")
-    
-    SentrySDK.start { options in
-        options.dsn = "https://118cff4b239bd3e0ede8fd74aad9bf8f@o497846.ingest.sentry.io/4506027753668608"
-        options.debug = true // Enabled debug when first installing is always helpful
-        options.enableTracing = true
-
-        // Uncomment the following lines to add more data to your events
-        // options.attachScreenshot = true // This adds a screenshot to the error events
-        // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
-    }
-    
-    return true
   }
 }
