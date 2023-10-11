3b887b85c1dc2d5a50ea252d9458a0553e6f5b87
Trevor Elkins <trevor@emergetools.com>
2023-10-11 16:10:14 -0400

    Remove XCTestCase

--- ios/Emerge Perf Tests/EmergePerfTests.swift
+++ ios/Emerge Perf Tests/EmergePerfTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class EmergePerfTest_TapsOnPostRow: XCTestCase, EMGPerfTest {
+final class EmergePerfTest_TapsOnPostRow: NSObject, EMGPerfTest {
   func runInitialSetup(withApp app: XCUIElement) {}
   
   func runIteration(withApp app: XCUIElement) {
@@ -15,12 +15,6 @@ final class EmergePerfTest_TapsOnPostRow: XCTestCase, EMGPerfTest {
     loginButton.tap()
     
     let firstRow = app.cells.element(boundBy: 0)
-    let exists = NSPredicate(format: "exists == true")
-    
-    expectation(for: exists, evaluatedWith: firstRow, handler: nil)
-    waitForExpectations(timeout: 5, handler: nil)
-    
-    XCTAssertTrue(firstRow.exists, "First row does not exist")
     firstRow.tap()
   }
 }
