3312e67d92936d336543c28329138827f311a10b
Trevor Elkins <trevor@emergetools.com>
2023-11-09 11:47:40 -0500

    test

--- ios/Hacker News/Components/StoryRow.swift
+++ ios/Hacker News/Components/StoryRow.swift
@@ -102,7 +102,7 @@ struct StoryRow_Preview: PreviewProvider {
     Group {
       StoryRow(appState: appState, story: mockStory, index: 0)
         .previewDevice(PreviewDevice(rawValue: "iPhone 14"))
-        .previewDisplayName("iPhone 14 test5")
+        .previewDisplayName("iPhone 14 test6")
         .previewLayout(.sizeThatFits)
       
       StoryRow(appState: appState, story: mockStory, index: 0)
