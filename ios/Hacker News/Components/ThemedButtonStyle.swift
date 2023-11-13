68b1ab651e47ddb3f0a45375e8ebbd468eeb5f09
Trevor Elkins <trevor@emergetools.com>
2023-11-13 14:49:55 -0500

    Add preview with crash

--- ios/Hacker News/Components/ThemedButtonStyle.swift
+++ ios/Hacker News/Components/ThemedButtonStyle.swift
@@ -17,3 +17,13 @@ struct ThemedButtonStyle: ButtonStyle {
       .cornerRadius(10)
   }
 }
+
+struct ThemedButtonStyle_PreviewProvider: PreviewProvider {
+  static var previews: some View {
+    var test = ["one", "two"]
+    var string = test[3]
+    Button(string) {}
+      .buttonStyle(ThemedButtonStyle())
+      .previewLayout(.sizeThatFits)
+  }
+}
