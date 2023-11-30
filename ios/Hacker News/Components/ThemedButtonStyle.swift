810ec8198b8704648698685a84f7498ac1999e44
Trevor Elkins <trevor@emergetools.com>
2023-11-30 11:38:57 -0500

    more test

--- ios/Hacker News/Components/ThemedButtonStyle.swift
+++ ios/Hacker News/Components/ThemedButtonStyle.swift
@@ -28,6 +28,76 @@ public struct ThemedButtonStyle2: ButtonStyle {
   }
 }
 
+public struct ThemedButtonStyle3: ButtonStyle {
+  public func makeBody(configuration: Self.Configuration) -> some View {
+    configuration.label
+      .foregroundColor(.white)
+      .padding()
+      .background(HNColors.orange)
+      .cornerRadius(10)
+  }
+}
+
+public struct ThemedButtonStyle4: ButtonStyle {
+  public func makeBody(configuration: Self.Configuration) -> some View {
+    configuration.label
+      .foregroundColor(.white)
+      .padding()
+      .background(HNColors.orange)
+      .cornerRadius(10)
+  }
+}
+
+public struct ThemedButtonStyle5: ButtonStyle {
+  public func makeBody(configuration: Self.Configuration) -> some View {
+    configuration.label
+      .foregroundColor(.white)
+      .padding()
+      .background(HNColors.orange)
+      .cornerRadius(10)
+  }
+}
+
+public struct ThemedButtonStyle6: ButtonStyle {
+  public func makeBody(configuration: Self.Configuration) -> some View {
+    configuration.label
+      .foregroundColor(.white)
+      .padding()
+      .background(HNColors.orange)
+      .cornerRadius(10)
+  }
+}
+
+public struct ThemedButtonStyle7: ButtonStyle {
+  public func makeBody(configuration: Self.Configuration) -> some View {
+    configuration.label
+      .foregroundColor(.white)
+      .padding()
+      .background(HNColors.orange)
+      .cornerRadius(10)
+  }
+}
+
+public struct ThemedButtonStyle8: ButtonStyle {
+  public func makeBody(configuration: Self.Configuration) -> some View {
+    configuration.label
+      .foregroundColor(.white)
+      .padding()
+      .background(HNColors.orange)
+      .cornerRadius(10)
+  }
+}
+
+public struct ThemedButtonStyle9: ButtonStyle {
+  public func makeBody(configuration: Self.Configuration) -> some View {
+    configuration.label
+      .foregroundColor(.white)
+      .padding()
+      .background(HNColors.orange)
+      .cornerRadius(10)
+  }
+}
+
 struct ThemedButtonStyle_PreviewProvider: PreviewProvider {
   static var previews: some View {
 //    fatalError("Test error")
