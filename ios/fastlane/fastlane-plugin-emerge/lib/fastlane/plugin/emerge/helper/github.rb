8089eaebd4b657a82d29cc0da91e5377c5dfebf5
Trevor Elkins <trevor@emergetools.com>
2024-03-08 13:27:55 -0500

    fix

--- ios/fastlane/fastlane-plugin-emerge/lib/fastlane/plugin/emerge/helper/github.rb
+++ ios/fastlane/fastlane-plugin-emerge/lib/fastlane/plugin/emerge/helper/github.rb
@@ -44,13 +44,13 @@ module Fastlane
         if is_push?
           ENV['GITHUB_SHA']
         elsif is_pull_request?
-          pull_request_event_data.dig(:pr, :head, :sha)
+          pull_request_event_data.dig(:pull_request, :head, :sha)
         end
       end
 
       def self.base_sha
         if is_pull_request?
-          pull_request_event_data.dig(:pr, :base, :sha)
+          pull_request_event_data.dig(:pull_request, :base, :sha)
         end
       end
 
