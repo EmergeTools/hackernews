7950a059764bd6903958f2d625dca36c9232607f
Rikin Marfatia <rikin@supergooey.dev>
2024-07-12 12:15:29 -0700

    --wip-- [skip ci]

--- android/app/src/main/java/com/emergetools/hackernews/MainActivity.kt
+++ android/app/src/main/java/com/emergetools/hackernews/MainActivity.kt
@@ -9,15 +9,29 @@ import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.IntOffset
+import androidx.navigation.Navigation
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.emergetools.hackernews.features.comments.commentsRoutes
 import com.emergetools.hackernews.features.stories.Stories
 import com.emergetools.hackernews.features.stories.storiesGraph
+import com.emergetools.hackernews.ui.theme.HNOrange
+import com.emergetools.hackernews.ui.theme.HNOrangeLight
 import com.emergetools.hackernews.ui.theme.HackerNewsTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +50,40 @@ class MainActivity : ComponentActivity() {
 fun App() {
   val navController = rememberNavController()
 
-  Scaffold() { innerPadding ->
+  Scaffold(
+    bottomBar = {
+      NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+        contentColor = MaterialTheme.colorScheme.onSurface
+      ) {
+        NavigationBarItem(
+          selected = true,
+          onClick = {},
+          icon = {
+            Icon(
+              imageVector = Icons.Rounded.Menu,
+              contentDescription = "Feed"
+            )
+          },
+          colors = NavigationBarItemDefaults.colors(
+            unselectedIconColor = MaterialTheme.colorScheme.onSurface,
+            selectedIconColor = MaterialTheme.colorScheme.onSurface,
+            indicatorColor = HNOrangeLight
+          )
+        )
+        NavigationBarItem(
+          selected = false,
+          onClick = {},
+          icon = {
+            Icon(
+              imageVector = Icons.Rounded.Settings,
+              contentDescription = "Settings"
+            )
+          }
+        )
+      }
+    }
+  ) { innerPadding ->
     NavHost(
       modifier = Modifier
         .fillMaxSize()
