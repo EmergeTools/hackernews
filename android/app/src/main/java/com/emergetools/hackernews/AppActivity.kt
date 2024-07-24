82c7a9c86a7c3df4ec0da5415f24152f401b23bb
Rikin Marfatia <rikin@supergooey.dev>
2024-07-24 14:56:57 -0700

    Fix Build

--- android/app/src/main/java/com/emergetools/hackernews/AppActivity.kt
+++ android/app/src/main/java/com/emergetools/hackernews/AppActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.navigation.ModalBottomSheetLayout
-import androidx.compose.material.navigation.rememberBottomSheetNavigator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -20,14 +16,11 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
@@ -35,8 +28,6 @@ import androidx.navigation.Navigator
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.emergetools.hackernews.data.ChromeTabsProvider
-import com.emergetools.hackernews.data.LocalCustomTabsIntent
-import com.emergetools.hackernews.features.bookmarks.BookmarksNavigation
 import com.emergetools.hackernews.features.bookmarks.bookmarksRoutes
 import com.emergetools.hackernews.features.comments.commentsRoutes
 import com.emergetools.hackernews.features.login.loginRoutes
@@ -45,9 +36,6 @@ import com.emergetools.hackernews.features.stories.Stories
 import com.emergetools.hackernews.features.stories.StoriesDestinations.Feed
 import com.emergetools.hackernews.features.stories.storiesGraph
 import com.emergetools.hackernews.ui.theme.HackerNewsTheme
-import com.emergetools.hackernews.ui.theme.HackerOrangeLight
-import com.emergetools.hackernews.ui.theme.HackerRed
-import com.emergetools.hackernews.ui.theme.HackerRedLight
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,64 +67,58 @@ fun rememberNavController(
 fun App() {
   val model = viewModel<AppViewModel>()
   val state by model.state.collectAsState()
-  val bottomSheetNavigator = rememberBottomSheetNavigator()
-  val navController = rememberNavController(bottomSheetNavigator) { destination ->
+  val navController = rememberNavController { destination ->
     model.actions(AppAction.DestinationChanged(destination))
   }
 
-  ModalBottomSheetLayout(
-    bottomSheetNavigator = bottomSheetNavigator,
-    sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
-  ) {
-    Scaffold(
-      bottomBar = {
-        NavigationBar {
-          state.navItems.forEach { navItem ->
-            NavigationBarItem(
-              selected = navItem.selected,
-              colors = NavigationBarItemDefaults.colors(
-                selectedIconColor = MaterialTheme.colorScheme.primary,
-                indicatorColor = MaterialTheme.colorScheme.primaryContainer
-              ),
-              onClick = {
-                model.actions(AppAction.NavItemSelected(navItem))
+  Scaffold(
+    bottomBar = {
+      NavigationBar {
+        state.navItems.forEach { navItem ->
+          NavigationBarItem(
+            selected = navItem.selected,
+            colors = NavigationBarItemDefaults.colors(
+              selectedIconColor = MaterialTheme.colorScheme.primary,
+              indicatorColor = MaterialTheme.colorScheme.primaryContainer
+            ),
+            onClick = {
+              model.actions(AppAction.NavItemSelected(navItem))
 
-                navController.navigate(navItem.route) {
-                  popUpTo<Feed> {
-                    saveState = true
-                  }
-                  launchSingleTop = true
-                  restoreState = true
+              navController.navigate(navItem.route) {
+                popUpTo<Feed> {
+                  saveState = true
                 }
-              },
-              icon = {
-                Icon(
-                  painter = painterResource(navItem.icon),
-                  contentDescription = navItem.label
-                )
-              },
-            )
-          }
+                launchSingleTop = true
+                restoreState = true
+              }
+            },
+            icon = {
+              Icon(
+                painter = painterResource(navItem.icon),
+                contentDescription = navItem.label
+              )
+            },
+          )
         }
       }
-    ) { innerPadding ->
-      NavHost(
-        modifier = Modifier
-          .fillMaxSize()
-          .padding(innerPadding),
-        navController = navController,
-        enterTransition = { slideIn { IntOffset(x = it.width, y = 0) } },
-        exitTransition = { slideOut { IntOffset(x = -it.width / 3, y = 0) } + fadeOut() },
-        popEnterTransition = { slideIn { IntOffset(x = -it.width, y = 0) } },
-        popExitTransition = { slideOut { IntOffset(x = it.width, y = 0) } },
-        startDestination = Stories
-      ) {
-        storiesGraph(navController)
-        commentsRoutes()
-        bookmarksRoutes(navController)
-        settingsRoutes(navController)
-        loginRoutes(navController)
-      }
+    }
+  ) { innerPadding ->
+    NavHost(
+      modifier = Modifier
+        .fillMaxSize()
+        .padding(innerPadding),
+      navController = navController,
+      enterTransition = { slideIn { IntOffset(x = it.width, y = 0) } },
+      exitTransition = { slideOut { IntOffset(x = -it.width / 3, y = 0) } + fadeOut() },
+      popEnterTransition = { slideIn { IntOffset(x = -it.width, y = 0) } },
+      popExitTransition = { slideOut { IntOffset(x = it.width, y = 0) } },
+      startDestination = Stories
+    ) {
+      storiesGraph(navController)
+      commentsRoutes()
+      bookmarksRoutes(navController)
+      settingsRoutes(navController)
+      loginRoutes(navController)
     }
   }
 }
