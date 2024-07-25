fe0ab0fc7e347cf6c34c07777e8c2067c045c2c2
Rikin Marfatia <rikin@supergooey.dev>
2024-07-25 10:00:59 -0700

    Fixing a crash when loading Feeds
    Apparently there is a chance that the API just straight up returns null as a valid response.
    In this scenario our JsonDeserializer fails and crashes the app. And in the case that this value is in the first page of the feed,
    we will constantly crash on startup.
    
    In order to safeguard against this, I just created a wrapper type and a custom deserializer that just checks if a gauranteed property
    exists in the json response, and if it does, use the normal item deserializer, otherwise map it to an empty object that is mostly a marker
    type, we can just ignore it.

--- android/app/src/main/java/com/emergetools/hackernews/data/HackerNewsBaseDataSource.kt
+++ android/app/src/main/java/com/emergetools/hackernews/data/HackerNewsBaseDataSource.kt
@@ -2,6 +2,9 @@ package com.emergetools.hackernews.data
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonContentPolymorphicSerializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -11,19 +14,32 @@ import retrofit2.http.Path
 
 const val BASE_FIREBASE_URL = "https://hacker-news.firebaseio.com/v0/"
 
-@Serializable
-data class Item(
-  val id: Long,
-  val type: String,
-  val time: Long,
-  val by: String? = null,
-  val title: String? = null,
-  val score: Int? = null,
-  val url: String? = null,
-  val descendants: Int? = null,
-  val kids: List<Long>? = null,
-  val text: String? = null
-)
+@Serializable(with = BaseResponseSerializer::class)
+sealed interface BaseResponse {
+  @Serializable
+  data class Item(
+    val id: Long,
+    val type: String,
+    val time: Long,
+    val by: String? = null,
+    val title: String? = null,
+    val score: Int? = null,
+    val url: String? = null,
+    val descendants: Int? = null,
+    val kids: List<Long>? = null,
+    val text: String? = null
+  ): BaseResponse
+
+  @Serializable
+  data object NullResponse: BaseResponse
+}
+
+object BaseResponseSerializer : JsonContentPolymorphicSerializer<BaseResponse>(BaseResponse::class) {
+  override fun selectDeserializer(element: JsonElement) = when {
+    "id" in element.jsonObject -> BaseResponse.Item.serializer()
+    else -> BaseResponse.NullResponse.serializer()
+  }
+}
 
 interface HackerNewsBaseApi {
   @GET("topstories.json")
@@ -33,7 +49,7 @@ interface HackerNewsBaseApi {
   suspend fun getNewStoryIds(): List<Long>
 
   @GET("item/{id}.json")
-  suspend fun getItem(@Path("id") itemId: Long): Item
+  suspend fun getItem(@Path("id") itemId: Long): BaseResponse
 }
 
 class HackerNewsBaseDataSource(json: Json, client: OkHttpClient) {
--- android/app/src/main/java/com/emergetools/hackernews/features/comments/CommentsDomain.kt
+++ android/app/src/main/java/com/emergetools/hackernews/features/comments/CommentsDomain.kt
@@ -16,7 +16,6 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.json.JsonNull.content
 import java.time.OffsetDateTime
 
 sealed interface CommentsState {
