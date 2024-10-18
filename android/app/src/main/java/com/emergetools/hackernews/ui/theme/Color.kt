112cc4ec3060db75a74edbfaac648c4fc626f1b0
Nico Hinderling <NicoHinderling@users.noreply.github.com>
2024-10-18 15:39:52 -0700

    color seven

--- android/app/src/main/java/com/emergetools/hackernews/ui/theme/Color.kt
+++ android/app/src/main/java/com/emergetools/hackernews/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.emergetools.hackernews.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val HackerOrange = Color(0xFFFF5733)
+val HackerOrange = Color(0xFF1D8348)
 val HackerOrangeLight = Color(0xFFFCE0C9)
 val HackerRed = Color(0xFFEF476F)
 val HackerBlue = Color(0xFF118AB2)
