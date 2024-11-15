9140c999400189490e273cb14b9db9d9a1cde100
Nico Hinderling <NicoHinderling@users.noreply.github.com>
2024-11-15 08:54:36 -0800

    [ignore] Testing snapshot approval

--- ios/HackerNews/Screens/LoginScreen.swift
+++ ios/HackerNews/Screens/LoginScreen.swift
@@ -15,7 +15,7 @@ struct LoginScreen: View {
   var body: some View {
     VStack(spacing: 20) {
       VStack(alignment: .leading) {
-        Text("Welcome to Hacker News")
+        Text("Welcome to Hacker News (Nico Intentionally Change the snapshot)")
           .font(.title)
         Text("Login to browse stories")
       }
