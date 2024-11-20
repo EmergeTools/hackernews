41add649675437cad950d28411efa5c260e62719
Trevor Elkins <trevor@emergetools.com>
2024-11-19 19:14:47 -0500

    Update

--- ios/HackerNews/Utils/Previews.swift
+++ ios/HackerNews/Utils/Previews.swift
@@ -6,8 +6,8 @@
 //
 
 import Foundation
-import SwiftUI
 import SnapshotPreferences
+import SwiftUI
 
 struct PreviewVariants<Content: View>: View {
   let content: Content
@@ -31,7 +31,7 @@ struct PreviewVariants<Content: View>: View {
         .navigationBarHidden(true)
         .previewDevice("iPhone 11 Pro Max")
         .previewDisplayName("iPhone 11 Pro Max, dark mode")
-      
+
       self.content
         .environment(\.colorScheme, .light)
         .preferredColorScheme(.light)
@@ -45,7 +45,7 @@ struct PreviewVariants<Content: View>: View {
         .navigationBarHidden(true)
         .previewDevice("iPhone 8")
         .previewDisplayName("iPhone 8, dark mode")
-      
+
       self.content
         .environment(\.colorScheme, .light)
         .preferredColorScheme(.light)
@@ -59,7 +59,7 @@ struct PreviewVariants<Content: View>: View {
         .navigationBarHidden(true)
         .previewDevice("iPad Air (5th generation)")
         .previewDisplayName("iPad Air, dark mode")
-      
+
       self.content
         .environment(\.colorScheme, .light)
         .preferredColorScheme(.light)
@@ -72,12 +72,20 @@ struct PreviewVariants<Content: View>: View {
 }
 
 struct PreviewHelpers {
-  static func withNavigationView(@ViewBuilder content: () -> some View) -> some View {
+  private static var referenceTimestamp: Int64 {
+    let calendar = Calendar.current
+    let oneYearAgo = calendar.date(byAdding: .hour, value: -1, to: Date())!
+    return Int64(oneYearAgo.timeIntervalSince1970)
+  }
+
+  static func withNavigationView(@ViewBuilder content: () -> some View)
+    -> some View
+  {
     NavigationStack {
       ZStack {
         HNColors.background
           .ignoresSafeArea()
-        
+
         content()
       }
       .toolbarColorScheme(.dark, for: .navigationBar)
@@ -92,11 +100,13 @@ struct PreviewHelpers {
     }
   }
 
-  static func makeFakeStory(index: Int64 = 0, descendants: Int = 0, kids: [Int64]? = nil) -> Story {
+  static func makeFakeStory(
+    index: Int64 = 0, descendants: Int = 0, kids: [Int64]? = nil
+  ) -> Story {
     return Story(
       id: index,
       by: "dang",
-      time: Int64(Date().timeIntervalSince1970) - Int64(index),
+      time: referenceTimestamp - (index * 3600),  // Each story 1 hour apart
       type: .story,
       title: "Test story \(index)",
       text: "Test story body \(index)",
@@ -111,22 +121,24 @@ struct PreviewHelpers {
     Comment(
       id: 1,
       by: "dang",
-      time: Int64(Date.now.timeIntervalSince1970),
+      time: referenceTimestamp,
       type: .comment,
       text: """
-  Totally useless commentary:
-  It makes me deeply happy to hear success stories like this for a project that's moving in the correctly opposite direction to that of the rest of the world.
+        Totally useless commentary:
+        It makes me deeply happy to hear success stories like this for a project that's moving in the correctly opposite direction to that of the rest of the world.
 
-  Engildification. Of which there should be more!
+        Engildification. Of which there should be more!
 
-  My soul was also satisfied by the Sleeping At Night post which, along with the recent "Lie Still in Bed" article, makes for very simple options to attempt to fix sleep (discipline) issues
-  """,
+        My soul was also satisfied by the Sleeping At Night post which, along with the recent "Lie Still in Bed" article, makes for very simple options to attempt to fix sleep (discipline) issues
+        """,
       parent: nil,
       kids: nil
     )
   }
 
-  static func makeFakeFlattenedComment(comment: Comment = makeFakeComment(), depth: Int = 0) -> FlattenedComment {
+  static func makeFakeFlattenedComment(
+    comment: Comment = makeFakeComment(), depth: Int = 0
+  ) -> FlattenedComment {
     FlattenedComment(comment: comment, depth: depth)
   }
 }
