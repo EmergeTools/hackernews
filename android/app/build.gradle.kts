e92d053bc68340664446d8a457402489d41f9fb1
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2024-11-20 21:02:20 -0800

    Prepare for Hacker News release 1.0.2 (#237)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     applicationId = "com.emergetools.hackernews"
     minSdk = 30
     targetSdk = 34
-    versionCode = 10
-    versionName = "1.0.1"
+    versionCode = 11
+    versionName = "1.0.2"
 
     manifestPlaceholders["emerge.distribution.apiKey"] = ""
 
