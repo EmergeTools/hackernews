25a35c61cdd79a32c55e09a9b4425b137de05dba
Rikin Marfatia <rikin@supergooey.dev>
2024-11-25 14:01:26 -0800

    Added Placeholders + Tab Bar

---
+++ ios/HackerNews/Screens/SettingsScreen.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsScreen.swift
+//  HackerNews
+//
+//  Created by Rikin Marfatia on 11/22/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct SettingsScreen: View {
+  var body: some View {
+    Text("Hello Settings")
+  }
+}
