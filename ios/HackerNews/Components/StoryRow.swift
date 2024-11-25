f3a9628f6a9cf89aeeb88473a09a23041ecd673b
Rikin Marfatia <rikin@supergooey.dev>
2024-11-25 15:00:56 -0800

    Replaced Story Row with New One

--- ios/HackerNews/Components/StoryRow.swift
+++ ios/HackerNews/Components/StoryRow.swift
@@ -9,77 +9,45 @@ import Foundation
 import SwiftUI
 
 struct StoryRow: View {
-  @ObservedObject var appState: AppViewModel
+  @ObservedObject var model: AppViewModel
   let story: Story
-  let index: Int
   
   var body: some View {
-    VStack(alignment: .leading) {
-      HStack {
+    VStack(alignment: .leading, spacing: 8) {
+      let author = story.by!
+      Text("@\(author)")
+        .foregroundColor(.hnOrange)
+        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+      Text(story.title)
+        .font(.headline)
+      HStack(spacing: 16) {
+        HStack(spacing: 4) {
+          Image(systemName: "arrow.up")
+            .foregroundColor(.green)
+          Text("\(story.score)")
+        }
+        HStack(spacing: 4) {
+          Image(systemName: "clock")
+            .foregroundColor(.purple)
+          Text(story.displayableDate)
+        }
+        Spacer()
+        // Comment Button
         Button(action: {
-          // TODO: Add upvote action
-          print("Pressed upvote for \(story.title)")
+          print("Pressed comment button for: \(story.id)")
+          model.navigationPath.append(
+            AppViewModel.AppNavigation.storyComments(story: story)
+          )
         }) {
-          VStack {
-            Image(systemName: "arrow.up")
-              .foregroundColor(Color.primary)
-            Text("\(story.score)")
-              .font(.caption)
-              .foregroundColor(Color.primary)
+          HStack(spacing: 4) {
+            Image(systemName: "message.fill")
+            Text("\(story.commentCount)")
           }
         }
-        .padding(.trailing, 4)
-        
-        VStack(alignment: .leading) {
-          Text("\(index + 1). \(story.title)")
-            .font(.subheadline)
-            .foregroundColor(Color.primary)
-          
-          if let displayableUrl = story.displayableUrl {
-            Text("(\(displayableUrl))")
-              .font(.caption2)
-              .foregroundColor(Color.primary.opacity(0.6))
-          }
-          HStack {
-            let dateAndAuthor: String = {
-              if let author = story.by {
-                return "\(story.displayableDate) by \(author)"
-              } else {
-                return story.displayableDate
-              }
-            }()
-            Text(dateAndAuthor)
-              .font(.caption)
-              .foregroundColor(Color.primary.opacity(0.6))
-            
-            if story.commentCount > 0 {
-              Button(action: {
-                appState.navigationPath.append(AppViewModel.AppNavigation.storyComments(story: story))
-              }) {
-                let commentText: String = {
-                  if story.commentCount == 1 {
-                    return "\(story.commentCount) comment"
-                  } else {
-                    return "\(story.commentCount) comments"
-                  }
-                }()
-                Text(commentText)
-                  .font(.caption)
-                  .fontWeight(.medium)
-                  .underline()
-                  .foregroundColor(Color.primary)
-              }
-              .buttonStyle(.borderedProminent)
-              .tint(HNColors.commentBackground)
-              .frame(maxWidth: .infinity, alignment: .trailing)
-            }
-          }
-          .padding(.horizontal, 2)
-          .padding(.top, 2)
-        }
+        .buttonStyle(.bordered)
+        .buttonBorderShape(ButtonBorderShape.capsule)
       }
     }
-    .padding(.top, 4)
   }
 }
 
@@ -87,7 +55,7 @@ struct StoryRow_Preview: PreviewProvider {
   static var previews: some View {
     let fakeStory = PreviewHelpers.makeFakeStory(index: 0, descendants: 3, kids: [1, 2, 3])
     PreviewVariants {
-      StoryRow(appState: AppViewModel(), story: fakeStory, index: 0)
+      StoryRow(model: AppViewModel(), story: fakeStory)
     }
   }
 }
--- ios/HackerNews/Screens/PostListScreen.swift
+++ ios/HackerNews/Screens/PostListScreen.swift
@@ -29,7 +29,7 @@ struct PostListScreen: View {
                 }
             }()
             
-            StoryRowV2(
+            StoryRow(
                 model: appState,
                 story: story
             )
