7c62182e1f26adf69cec0e63ece7b9e4d1322754
Nelson Osacky <nelson@emergetools.com>
2025-01-15 16:20:21 -0500

    Add Develocity Gradle Plugin

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -11,6 +11,18 @@ pluginManagement {
     gradlePluginPortal()
   }
 }
+
+plugins {
+  id("com.gradle.develocity") version("3.19")
+}
+
+develocity {
+  buildScan {
+    termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+    termsOfUseAgree.set("yes")
+  }
+}
+
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
