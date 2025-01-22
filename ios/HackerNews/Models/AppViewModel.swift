e5cc29aa9faafb43c80fbda188ffc4163da2868c
Rikin Marfatia <rikin@supergooey.dev>
2025-01-22 12:22:41 -0800

    Settings Screen Design (#306)
    Adding the New Settings Screen Components:
    - Login Row
    - Settings Row for Links
    
    ---------
    
    Co-authored-by: Trevor Elkins <trevor@emergetools.com>

--- ios/HackerNews/Models/AppViewModel.swift
+++ ios/HackerNews/Models/AppViewModel.swift
@@ -225,6 +225,10 @@ class AppViewModel: ObservableObject {
     }
   }
 
+  func openLink(url: URL) {
+    navigationPath.append(AppNavigation.webLink(url: url, title: ""))
+  }
+
   func toggleBookmark(_ item: StoryContent) {
     feedState.stories = feedState.stories.map { current in
       if case .loaded(let content) = current {
@@ -247,7 +251,7 @@ class AppViewModel: ObservableObject {
     }
   }
 
-  func loginTapped(username: String, password: String) async {
+  func loginSubmit(username: String, password: String) async {
     let status = await webClient.login(acct: username, pw: password   )
     print("Login Status: \(status)")
     switch status {
