3ca9ee0ebd20e3e9b77b6733b951f3f315ee43ef
Trevor Elkins <trevor@emergetools.com>
2025-01-24 17:55:25 -0500

    Add swipe navigation to comment screen

---
+++ ios/HackerNews/Utils/UINavigationController+SwipeBack.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+  override open func viewDidLoad() {
+    super.viewDidLoad()
+    interactivePopGestureRecognizer?.delegate = self
+  }
+
+  public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+    return viewControllers.count > 1
+  }
+
+  // This makes it work properly with ScrollView
+  public func gestureRecognizer(
+    _ gestureRecognizer: UIGestureRecognizer,
+    shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+  ) -> Bool {
+    return true
+  }
+}
