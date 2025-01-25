475898b9ec10886fecb1b63d861124084be592dc
Filipe Alvarenga <ofilipealvarenga@gmail.com>
2025-01-24 21:03:46 -0300

    Add workaround to enable the interactive pop gesture on SwiftUI's NavigationStack

--- ios/HackerNews/Utils/Extensions.swift
+++ ios/HackerNews/Utils/Extensions.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 import SwiftSoup
+import UIKit
+
+/// This code provides a workaround to enable the interactive pop gesture in NavigationStacks in SwiftUI.
+/// While not the most elegant or ideal solution, it significantly enhances the navigation user experience.
+///
+/// Note: This is intended as a temporary fix and should be removed once Apple adds proper support
+/// for interactive pop gestures in NavigationStacks. Please refer to PR #TBD for more details.
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+  override open func viewDidLoad() {
+    super.viewDidLoad()
+    interactivePopGestureRecognizer?.delegate = self
+  }
+
+  public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+    return viewControllers.count > 1
+  }
+}
 
 extension String {
   func strippingHTML() -> String {
