e5b2a5d692b05de38fcc264d9185e942b3b4d87d
Trevor Elkins <trevor@emergetools.com>
2025-04-28 15:13:04 -0400

    remove spring

--- ios/HackerNews/Comments/CommentRow.swift
+++ ios/HackerNews/Comments/CommentRow.swift
@@ -102,13 +102,16 @@ struct CommentRow: View {
       }
     }
     .clipShape(RoundedRectangle(cornerRadius: 16.0))
-    .animation(.spring(duration: 0.3), value: state.hidden)
     .padding(
       EdgeInsets(
         top: 0,
-        leading: min(CGFloat(state.level * 20), CGFloat(maxIndentationLevel * 20)),
+        leading: min(
+          CGFloat(state.level * 20),
+          CGFloat(maxIndentationLevel * 20)
+        ),
         bottom: 0,
-        trailing: 0)
+        trailing: 0
+      )
     )
   }
 }
