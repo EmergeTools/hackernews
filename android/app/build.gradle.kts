252e0f706045ff5bbba1737337be3eb307716196
Nelson Osacky <nelson@emergetools.com>
2025-09-16 18:55:39 +0200

    Revert "Add base SHA configuration to VCSInfoExtension"
    This reverts commit 26b3cc57cc57bf1ee07e4dfe694f0188126683ce.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -142,7 +142,6 @@ sentry {
   vcsInfo {
     System.getenv("GITHUB_HEAD_REF")?.let { headRef.set(it) }
     System.getenv("GITHUB_BASE_REF")?.let { baseRef.set(it) }
-    System.getenv("GITHUB_EVENT_PULL_REQUEST_BASE_SHA")?.let { baseSha.set(it) }
     headRepoName.set(System.getenv("GITHUB_REPOSITORY") ?: "EmergeTools/hackernews")
     baseRepoName.set(System.getenv("GITHUB_REPOSITORY") ?: "EmergeTools/hackernews")
     vcsProvider.set("github")
