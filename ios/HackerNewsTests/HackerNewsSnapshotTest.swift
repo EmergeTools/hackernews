5353f8fd8d90002fc1d347ae747a0394b77965fb
Noah Martin <noahm444@gmail.com>
2025-11-14 08:29:12 -0800

    Support new a11y snapshot API

--- ios/HackerNewsTests/HackerNewsSnapshotTest.swift
+++ ios/HackerNewsTests/HackerNewsSnapshotTest.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 import SnapshottingTests
+#if canImport(UIKit)
+import UIKit
+#endif
+#if canImport(AccessibilitySnapshotCore)
+import AccessibilitySnapshotCore
+#endif
 
 class HackerNewsSnapshotTest: SnapshotTest {
   override class func snapshotPreviews() -> [String]? {
@@ -15,5 +21,38 @@ class HackerNewsSnapshotTest: SnapshotTest {
 
   override class func excludedSnapshotPreviews() -> [String]? {
     return nil
+  }
+  
+#if canImport(UIKit) && !os(watchOS) && !os(visionOS) && !os(tvOS)
+override open class func setupA11y() -> ((UIViewController, UIWindow, PreviewLayout) -> UIView)? {
+  return { (controller: UIViewController, window: UIWindow, layout: PreviewLayout) in
+    let containerVC = controller.parent
+    let containedView: UIView
+    switch layout {
+    case .device:
+      containedView = containerVC?.view ?? controller.view
+    default:
+      containedView = controller.view
+    }
+    let a11yView = AccessibilitySnapshotView(
+      containedView: containedView,
+      viewRenderingMode: controller.view.bounds.size.requiresCoreAnimationSnapshot ? .renderLayerInContext : .drawHierarchyInRect,
+      activationPointDisplayMode: .never,
+      showUserInputLabels: true)
+  
+    a11yView.center = window.center
+    window.addSubview(a11yView)
+
+    _ = try? a11yView.parseAccessibility(useMonochromeSnapshot: false)
+    a11yView.sizeToFit()
+    return a11yView
+  }
+}
+#endif
+}
+
+extension CGSize {
+  var requiresCoreAnimationSnapshot: Bool {
+    height >= UIScreen.main.bounds.size.height * 2
   }
 }
